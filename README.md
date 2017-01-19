0a8204367628e27c9337d2d52097203d57c58022
Jonathan Pierson <Jonathan Pierson>
2017-01-18 22:05:47 -0500

    fleshed out a simple class. defined the basics of main.cpp. added details to README.md.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spgd
-senior project game design (ft. A.N.)
+senior project game design  
+joint project w/Abriana Nash
+
+## tools
+cygwin-b20 (what year is it?) (might be UTD, soon)  
+Atom text editor (atom.io)  
+Git GUI (git-scm.com/download)  
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+
+
+// the default class for any living entity in our game.
+// so far, they've got some basic stats computed with some basic formulae.
+// str: a measure of strength. determines hp and physical damage.
+// wis: a measure of wisdom. determines mp and magical damage.
+// def: a measure of defense. determines physical guard and boosts hp.
+// res: a measure of resistance. determines magical guard and boosts mp.
+// spd: a measure of speed. determines when the character acts.
+// lck: a measure of luck. determines probability of criticals.
+// xp: experience points. how many do you need?
+// lvl: the character's level.
+// name: what's in a name?
+
+// from jon to jon: move these to the readme, nerd!
+
+
+class Creature {
+  int hp,mp,str,wis,def,res,spd,lck,xp,lvl;
+  string name;
+public:
+  Creature();
+};
+
+Creature::Creature() {
+  str=1;
+  wis=1;
+  def=1;
+  res=1;
+  spd=1;
+  lck=1;
+  xp=0;
+  lvl=1;
+  name="character";
+}
+
+int main() {
+  srand(time(NULL));  // seed for randomness at the current time.
+  return 0;           // the very basics!
+}
